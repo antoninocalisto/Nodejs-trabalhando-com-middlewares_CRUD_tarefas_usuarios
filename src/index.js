5030d61844e8546c1f92b14ef65d35dac67527f4
antoninocalisto <antuninosantos@gmail.com>
2021-07-09 11:47:16 -0300

    Cadastro de usuarios e middlewares feitos

--- src/index.js
+++ src/index.js
@@ -11,18 +11,59 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const {username} = request.headers; 
+  const user = users.find(user => user.username === username);
+  
+  if(!user){
+    return response.status(404).json( {error: "User not found!"})
+  }
+
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const {user} = request;
+
+  if( (user.todos.length >= 10 && user.pro === false)){
+    return response.status(403).json({error: "User with plan free actived and more 10 users registred or user with plan pro actived!"});
+  }
+  
+  request.user = user;
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  const {username} = request.headers;
+  const {id} = request.params;
+
+  if(id.type !== uuidv4){
+    return response.status(400).json({error: "Id not is of uuidv4!"});
+  }
+
+  user = users.find(user => user.id === id);
+  if(!user){
+    return response.status(400).json({error: "User not found!"});
+  }
+
+    request.user = user;
+
+    return next();
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const {id} = request.params; 
+  const user = users.find(user => user.id === id);
+  
+  if(!user){
+    return response.status(400).json( {error: "User not found!"})
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
